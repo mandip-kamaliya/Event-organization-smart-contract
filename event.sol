1f672854cbba8c8309ff8f5c481fea92aa646d1d
mandip kamaliya <mandipkamaliya7@gmail.com>
2024-09-10 14:43:45 +0530

    add Buyticket and Transfertickets functions

--- event.sol
+++ event.sol
@@ -23,6 +23,22 @@ contract EventOrganisation{
     events[nextid]=Event(msg.sender,name,date,price,ticketCount,ticketCount);
     nextid++;
  }
+ function Buytickets(uint id,uint quantity) external payable{
+   require(events[id].date!=0,"event does not exists");
+   require(events[id].date>block.timestamp,"event is expired");
+   Event storage _event=events[id];
+   require(msg.value==(_event.price*quantity),"Ethers is not enough");
+   require(_event.ticketRemain>=quantity,"not enough tickets available");
+   _event.ticketRemain-=quantity;
+   tickets[msg.sender][id]+=quantity; 
+ }
+ function Transfertickets(uint id,uint quantity,address to) external payable {
+   require(events[id].date!=0,"event does not exits");
+   require(events[id].date>block.timestamp,"event is already comleted");
+   require(tickets[msg.sender][id]>=quantity,"dont have enough tickets to transfer");
 
+   tickets[msg.sender][id]-=quantity;
+   tickets[to][id]+=quantity;
+ } 
 
 }
